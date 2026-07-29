96bb829f68e67cbaf9e9b3c55f14f10617d62118
Vishal Babu <vishalbabu422@gmail.com>
2026-07-29 11:04:43 +0530

    feat: show side navigation based on user permissions on reload

--- BackEnd/modules/auth/authController.js
+++ BackEnd/modules/auth/authController.js
@@ -164,42 +164,11 @@ exports.logout = catchAsync(async (req, res) => {
 exports.getMe = catchAsync(async (req, res, next) => {
   const user = await Employee.findByPk(req.user.id, {
     attributes: { exclude: ["password"] },
-     include: [
-    {
-      model: RoleMaster,
-      attributes: ["role_code", "is_super_admin"],
-      include: [
-        {
-          model: Permission,
-          attributes: ["permission_key"],
-          through: { attributes: [] },
-          where: {
-            action: "Show Module",
-          },
-          required: false,
-        },
-      ],
-    },
-  ],
   });
 
   if (!user) {
     return next(new AppError("User not found", 404));
   }
-
-  const roles = user.RoleMasters || [];
-
-const permissionSet = new Set();
-
-roles.forEach((role) => {
-  role.Permissions?.forEach((permission) => {
-    permissionSet.add(permission.permission_key);
-  });
-});
-
-const permissions = [...permissionSet];
-const roleCodes = roles.map((role) => role.role_code);
-const isSuperAdmin = roles.some((role) => role.is_super_admin);
 
   res.status(200).json({
     status: "success",
@@ -216,9 +185,7 @@ const isSuperAdmin = roles.some((role) => role.is_super_admin);
       employee_code: user.employee_code,
       is_active: user.is_active,
       must_change_password: user.must_change_password,
-      isSuperAdmin,
-      role: roleCodes,
-      permissions,
+      isSuperAdmin: req.isSuperAdmin,
     },
   });
 });
